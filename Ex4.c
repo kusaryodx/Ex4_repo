280438a4ef0b520c9e90312f49d54cd3d233d499
kusaryodx <69282563+kusaryodx@users.noreply.github.com>
2020-08-06 16:34:40 +0900

    Create Ex4.c

---
+++ Ex4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+int i,num,h = 0,t = 0;
+printf("Tossing\n");
+for(i = 0;i < 3;i++){
+num=rand();
+if(num%2 == 0){
+printf("R %d:　Head\n", i+1);
+h++;
+}
+else{
+printf("R %d:　Tails\n", i+1);
+t++;
+}
+}
+printf("H:%d T:%d",h,t);
+return 0;
+}
